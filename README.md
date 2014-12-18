9dc86f3406e187ad73f36225afb173d1e97ab507
Peter Winton <pwinton@indigobio.com>
2014-12-18 13:24:27 -0500

    wip

--- README.md
+++ README.md
@@ -11,9 +11,13 @@ to inflect field names (or hash keys).
 # To do
 
 - embedded messages
-- repeated, optional
+- repeated
+  - packed
+- optional
 
 - test against another PB implementation
+
+- performance testing
 
 - hash support
   - consider being more flexible with determining if a target type should be treated as a hash
@@ -33,5 +37,23 @@ to inflect field names (or hash keys).
 
 - optimization
   - pointers vs references
+  - track average (or max) message byte-size per type.
+    - use to estimate byte size of varint embedded msg length.
+      if we guess correctly, we can write the embedded message in place
+      and back-fill the length, instead of writing to a temp buffer,
+      finding the length, then copying. Pad with one extra varint byte
+      of zeros to reduce likelihood of having to memmove.
+    - also use to estimate initial capacity of top level message.
+    - rethought:
+      remember last encoded size for each message type (on both write and read??),
+      as well as the number of bytes it took to encode the size as a varint.
+      (this is cheaper than averaging, and probably just as good)
+      - when writing a type:
+        - use the previous size as a basis for initializing the buffer capacity
+        - if embedded, allocate the previously required number of bytes
+          for the varint length.
+          - if this proves to be more than actually needed, pad with zero bits
+          - if not enough, insert the appropriate number of additional bytes.
+            (std::vector.insert() is likely performant enough for this)
 
 
--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -8,33 +8,13 @@ describe Pbr do
 
   context 'roundtrips' do
 
-    def self.it_roundtrips_int(bits, type)
-      it "#{type}" do
-        roundtrip(type, 0)
-        roundtrip(type, 1)
-        roundtrip(type, -1)
-        roundtrip(type, 1234)
-        roundtrip(type, 2**(bits-1) - 1)
-        roundtrip(type, -2**(bits-1))
-      end
-    end
-
-    def self.it_roundtrips_uint(bits, type)
-      it "#{type}" do
-        roundtrip(type, 0)
-        roundtrip(type, 1)
-        roundtrip(type, 1234)
-        roundtrip(type, 2**bits - 1)
-      end
-    end
-
-    def roundtrip(short_message_type, str, field_name=:foo, field_num=1)
-      v1, v2 = do_roundtrip(short_message_type, str, field_name=:foo, field_num=1)
+    def roundtrip(tripper, short_message_type, str, field_name=:foo, field_num=1)
+      v1, v2 = do_roundtrip(tripper, short_message_type, str, field_name=:foo, field_num=1)
       expect(v2).to eql v1
     end
 
-    def roundtrip_float(precision, short_message_type, str, field_name=:foo, field_num=1)
-      v1, v2 = do_roundtrip(short_message_type, str, field_name=:foo, field_num=1)
+    def roundtrip_float(tripper, precision, short_message_type, str, field_name=:foo, field_num=1)
+      v1, v2 = do_roundtrip(tripper, short_message_type, str, field_name=:foo, field_num=1)
       if v1 == 0
         expect(v2).to eql v1
       else
@@ -49,16 +29,33 @@ describe Pbr do
       end
     end
 
-    def do_roundtrip(short_message_type, str, field_name=:foo, field_num=1)
+    def do_roundtrip(tripper, short_message_type, str, field_name=:foo, field_num=1)
       message_type = msg_type(short_message_type, field_name, field_num)
-      obj = OpenStruct.new
-      obj.send("#{field_name}=", str)
-      bytes = Pbr.new.write(obj, message_type)
-      obj2  = Pbr.new.read(bytes, message_type)
-      v2    = obj2.send(field_name)
-      v1    = obj.send(field_name)
+      v1, v2 = tripper.call(message_type, str, field_name)
       shown = "#{v1.inspect}#{v1.is_a?(String) ? " (#{v1.encoding})" : ''} -> #{v2.inspect}#{v2.is_a?(String) ? " (#{v2.encoding})" : ''}"
       puts shown.size > 120 ? shown[0..100] + '...' : shown
+      [v1, v2]
+    end
+
+    def self.do_obj_roundtrip(message_type, field_val, field_name=:foo)
+      obj = OpenStruct.new
+      obj.send("#{field_name}=", field_val)
+      bytes = Pbr.new.write(obj, message_type)
+      obj2  = Pbr.new.read(bytes, message_type)
+      # expect(obj2).to be_a OpenStruct
+      v2    = obj2.send(field_name)
+      v1    = obj.send(field_name)
+      [v1, v2]
+    end
+
+    def self.do_hash_roundtrip(message_type, field_val, key_name=:foo)
+      obj = {}
+      obj[key_name] = field_val
+      bytes = Pbr.new.write(obj, message_type)
+      obj2  = Pbr.new.read(bytes, message_type)
+      # expect(obj2).to be_a Hash
+      v2    = obj2[key_name]
+      v1    = obj2[key_name]
       [v1, v2]
     end
 
@@ -67,68 +64,91 @@ describe Pbr do
       Pbr::TMessage.new('TestMsg', [Pbr::TField.new(field_name, field_num, field_type_class)])
     end
 
-    it 'field names' do
-      roundtrip(:string, 'sss', :foo)
-      roundtrip(:string, 'sss', 'foo')
+    def self.it_roundtrips_int(tripper, bits, type)
+      it "#{type}" do
+        roundtrip(tripper, type, 0)
+        roundtrip(tripper, type, 1)
+        roundtrip(tripper, type, -1)
+        roundtrip(tripper, type, 1234)
+        roundtrip(tripper, type, 2**(bits-1) - 1)
+        roundtrip(tripper, type, -2**(bits-1))
+      end
     end
 
-    it 'field numbers' do
-      min_field_num = 1
-      max_field_num = 2 ** 29 - 1
-      roundtrip(:string, 'sss', :foo, min_field_num)
-      roundtrip(:string, 'sss', :foo, 1234)
-      roundtrip(:string, 'sss', :foo, max_field_num)
+    def self.it_roundtrips_uint(tripper, bits, type)
+      it "#{type}" do
+        roundtrip(tripper, type, 0)
+        roundtrip(tripper, type, 1)
+        roundtrip(tripper, type, 1234)
+        roundtrip(tripper, type, 2**bits - 1)
+      end
     end
 
-    it 'string' do
-      roundtrip(:string, '')
-      roundtrip(:string, 'hello, world!')
-      roundtrip(:string, "hello\0world")
-      roundtrip(:string, 'z' * 1024 * 1024)
+    [method(:do_obj_roundtrip)].each do |t|
 
-      v1, v2 = do_roundtrip(:string, 'hello, world!'.encode('utf-16'))
-      expect(v1.encoding).to eql Encoding::UTF_16
-      expect(v2.encoding).to eql Encoding::UTF_8
-      expect(v2).to eql v1.encode('utf-8')
+      it 'field names' do
+        roundtrip(t, :string, 'sss', :foo)
+        roundtrip(t, :string, 'sss', 'foo')
+      end
+
+      it 'field numbers' do
+        min_field_num = 1
+        max_field_num = 2 ** 29 - 1
+        roundtrip(t, :string, 'sss', :foo, min_field_num)
+        roundtrip(t, :string, 'sss', :foo, 1234)
+        roundtrip(t, :string, 'sss', :foo, max_field_num)
+      end
+
+      it 'string' do
+        roundtrip(t, :string, '')
+        roundtrip(t, :string, 'hello, world!')
+        roundtrip(t, :string, "hello\0world")
+        roundtrip(t, :string, 'z' * 1024 * 1024)
+
+        v1, v2 = do_roundtrip(t, :string, 'hello, world!'.encode('utf-16'))
+        expect(v1.encoding).to eql Encoding::UTF_16
+        expect(v2.encoding).to eql Encoding::UTF_8
+        expect(v2).to eql v1.encode('utf-8')
+      end
+
+      it 'bytes' do
+        roundtrip(t, :bytes, 'hello')
+        roundtrip(t, :bytes, [0, 1, 2, 3, 255].pack('c*'))
+      end
+
+      it 'float' do
+        roundtrip_float(t, 7, :float, 0.0)
+        roundtrip_float(t, 7, :float, 3.14)
+        roundtrip_float(t, 7, :float, -3.14)
+        roundtrip_float(t, 7, :float, 3.402823e38)
+        roundtrip_float(t, 7, :float, -3.402823e38)
+      end
+
+      it 'double' do
+        roundtrip(t, :double, 0.0)
+        roundtrip(t, :double, 3.14)
+        roundtrip(t, :double, -3.14)
+        roundtrip(t, :double, 3.402823e38)
+        roundtrip(t, :double, -3.402823e38)
+        roundtrip(t, :double, 0.030000000000000006)
+      end
+
+      it 'bool' do
+        roundtrip(t, :bool, true)
+        roundtrip(t, :bool, false)
+      end
+
+      it_roundtrips_int(t, 32, :int32)
+      it_roundtrips_uint(t, 32, :uint32)
+      it_roundtrips_int(t, 64, :int64)
+      it_roundtrips_uint(t, 64, :uint64)
+      it_roundtrips_int(t, 32, :sint32)
+      it_roundtrips_int(t, 64, :sint64)
+      it_roundtrips_int(t, 32, :sfixed32)
+      it_roundtrips_uint(t, 32, :fixed32)
+      it_roundtrips_int(t, 64, :sfixed64)
+      it_roundtrips_uint(t, 64, :fixed64)
     end
-
-    it 'bytes' do
-      roundtrip(:bytes, 'hello')
-      roundtrip(:bytes, [0, 1, 2, 3, 255].pack('c*'))
-    end
-
-    it 'float' do
-      roundtrip_float(7, :float, 0.0)
-      roundtrip_float(7, :float, 3.14)
-      roundtrip_float(7, :float, -3.14)
-      roundtrip_float(7, :float, 3.402823e38)
-      roundtrip_float(7, :float, -3.402823e38)
-    end
-
-    it 'double' do
-      roundtrip(:double, 0.0)
-      roundtrip(:double, 3.14)
-      roundtrip(:double, -3.14)
-      roundtrip(:double, 3.402823e38)
-      roundtrip(:double, -3.402823e38)
-      roundtrip(:double, 0.030000000000000006)
-    end
-
-    it 'bool' do
-      roundtrip(:bool, true)
-      roundtrip(:bool, false)
-    end
-
-    it_roundtrips_int( 32, :int32)
-    it_roundtrips_uint(32, :uint32)
-    it_roundtrips_int( 64, :int64)
-    it_roundtrips_uint(64, :uint64)
-    it_roundtrips_int( 32, :sint32)
-    it_roundtrips_int( 64, :sint64)
-    it_roundtrips_int( 32, :sfixed32)
-    it_roundtrips_uint(32, :fixed32)
-    it_roundtrips_int( 64, :sfixed64)
-    it_roundtrips_uint(64, :fixed64)
   end
 
   C = Struct.new(:foo)
